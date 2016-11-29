56be8cc35fe57a2c57dd9b106e061f0c880795ce
Alex Napier <alex.r.napier@gmail.com>
2016-11-28 20:42:38 -0600

    added portfolio media item build in upon intro animation end

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 
 var wrapper = document.querySelector('.wrapper');
 var intro = document.querySelector('.intro');
+var portPage = document.querySelector('.page__portfolio');
+var mediaItems = [];
 
 setTimeout(function() {
   wrapper.removeChild(intro);
+  portPage.style.display = 'flex';
+  $( ".media" ).each(function( i ) {
+    console.log($(this));
+    $(this).css('animation', 'BuildIn .8s ease-out .6s forwards');
+  });
+  // for (i = 0; i < mediaItems.length; i++) {
+  //   mediaItems[i].style.animation = 'BuildIn .4s ease-out forwards';
+  //   console.log(mediaItems[i]);
+  // }
 }, 8200);
